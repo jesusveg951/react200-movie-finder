092f72e45fe358d6791abc2c01b4a40d944affa8
Jesus Vega <jesusvega00@gmail.com>
2020-01-28 15:18:52 -0800

    wip

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import reducers from './reducers';
 import App from './app';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(reducers, composeEnhancers(
+          applyMiddleware(
+            promiseMiddleware()
+          )
+        ));
+                
 /* eslint-enable */
-
-
 render(
   <Provider store={ store }>
     <App />
