75309f01fe55bb77f7e81722a5dc04490faf3903
Jesus Vega <jesusvega00@gmail.com>
2020-01-31 15:53:17 -0800

    wip

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import promise from 'redux-promise-middleware';
 import searchReducer from './containers/MovieSearchContainer/searchReducer';
 import App from './app';
